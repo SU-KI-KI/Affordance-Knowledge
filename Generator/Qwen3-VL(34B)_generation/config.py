785359d27633f269a42fd3bfcba10d0337af91b4
SU-KI-KI <s2426077@u.tsukuba.ac.jp>
2026-05-31 14:19:57 +0900

    Add files via upload

---
+++ Generator/Qwen3-VL(34B)_generation/config.py
@@ -0,0 +1,8 @@
+START_ROW = 1301
+END_ROW = 2000
+
+MODEL_NAME = "Qwen/Qwen3-VL-32B-Instruct"
+CACHE_DIR = "/data/SUYUKANG/huggingface/hub/"
+URL_CSV = "Data/500_URL.csv"
+OBJECT_JSON = "Data/All_object.json"
+PROMPT_PY = "prompt.py"
