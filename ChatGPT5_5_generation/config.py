52f17f31c148908140f8143074ab6eede8b98e34
SU-KI-KI <s2426077@u.tsukuba.ac.jp>
2026-05-31 14:14:53 +0900

    Add files via upload

---
+++ ChatGPT5_5_generation/config.py
@@ -0,0 +1,6 @@
+# Parameter configuration
+image_URL = 'Data/500_URL.csv'  # Dataset to use ('HCVRD'/'HICO-DET')
+object_list = 'Data/All_object.json'
+start_line = 12  # Starting line
+end_line = 12  # Ending line
+prompt = 'zeroshot'  # Select prompt ('fewshot'/'zeroshot')
