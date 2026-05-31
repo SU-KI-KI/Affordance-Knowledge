ddedda19e649da18d25edff3a92793dd492a99cf
SU-KI-KI <s2426077@u.tsukuba.ac.jp>
2026-05-31 14:18:56 +0900

    Delete Qwen2.5-VL(32B)_generation directory

--- Qwen2.5-VL(32B)_generation/config.py
+++
@@ -1,8 +0,0 @@
-START_ROW = 1301
-END_ROW = 2000
-
-MODEL_NAME = "Qwen/Qwen2.5-VL-32B-Instruct"
-CACHE_DIR = "/data01/SUYUKANG/huggingface/hub/"
-URL_CSV = "Data/500_URL.csv"
-OBJECT_JSON = "Data/All_object.json"
-PROMPT_PY = "prompt.py"
